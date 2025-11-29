857c8bd8fa92526c3289554f278c81bfcc263f76
Artyrka0312 <stadnikartyr@gmail.com>
2025-11-29 23:31:36 +0200

    ТЕМА: Classes. Properties Завдання 1: Розробити клас «Freezer», який описує морозильну камеру з дотриманням наступних вимог: 1. Реалізувати не менше п'яти закритих полів (різних типів), що представляють основні характеристики класу.
    2. Створити не менше двох статичних полів (різних типів), що представляють спільні характеристики
    об'єктів даного класу.
    3. Створити статичний конструктор для ініціалізації статичних полів.
    4. Обов'язковою вимогою є реалізація декількох перевантажених конструкторів,
    аргументи яких визначаються студентом,
    виходячи із специфіки реалізованого класу і
     так само реалізація конструктора за замовчуванням.
    По можливості застосувати делегування конструкторів.
    
    5. Перевизначити алгоритм методу ToString(),
    який буде повертати інформацію про об’єкт у вигляді рядка.
    
    6. Перенести описи всіх методів в інший файл, використовуючи partial class.
    
    7. Створити масив (не менше 5 елементів) об'єктів створеного класу та показати інформацію по кожному.

---
+++ Intro to OOP C#/Intro to OOP C#/FreezerMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro_to_OOP_C_
+{
+    public partial class Freezer
+    {
+        public void ToString()
+        {
+            Console.WriteLine($"Model: {this.model}, brand: {this.brand}, price: {this.price}, width: {this.width}, height: {this.height}, length: {this.lenght}");
+        }
+
+    }
+}
---
+++ Intro to OOP C#/Intro to OOP C#/Program.cs
@@ -0,0 +1,122 @@
+﻿namespace Intro_to_OOP_C_
+
+
+{
+    public partial class Freezer
+    {
+        private string brand { get; set; }
+        private string model { get; set; }
+        private int height { get; set; }
+        private int width { get; set; }
+
+        private int lenght { get; set; }
+
+        private int price { get; set; }
+        static public int count { get; private set; }
+
+        static public float AveragePrice { get; private set; }
+
+        static private float TotalPrice ;
+
+        static Freezer()
+        {
+            count = 0;
+            AveragePrice = 0;
+        }
+
+        public Freezer()
+        {
+            brand = "unknown";
+            price = 0;
+        }
+
+        public Freezer(string brand, int price)
+        {
+            this.brand = brand;
+            this.price = price;
+            count++;
+            TotalPrice += price;
+            AveragePrice = TotalPrice / count;
+        }
+
+        public Freezer (string model, int height, int width, int length, string brand, int price) : this(brand,price)
+        {
+            this.model = model;
+            this.height = height;
+            this.width = width;
+            this.lenght = length;
+            count++;
+            TotalPrice += price;
+            AveragePrice = TotalPrice / count;
+
+        }
+
+        
+
+
+
+    }
+
+
+
+    internal class Program
+    {
+        static void ArrayFreezer(List<Freezer> array)
+        {
+            foreach (Freezer freezer in array)
+            {
+                freezer.ToString();
+            }
+            return;
+        }
+        static void Main(string[] args)
+        {
+            int choose;
+            List<Freezer> array = new List<Freezer>();
+            while (true) {
+                Console.WriteLine("Choose an option: \n1.Add new freezer(default)\n2.Add new freezer(full info)\n3.Show full list of freezers\n0.Exit");
+                choose = int.Parse(Console.ReadLine());
+                switch (choose) {
+                    case 0: break;
+                    case 1:
+                        Console.Write("Type a brand: ");
+                        string brand = Console.ReadLine();
+                        Console.WriteLine();
+                        Console.Write("Type a price: ");
+                        int price = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        Freezer p = new Freezer(brand, price);
+                        array.Add(p);
+                        continue;
+                    case 2:
+                        Console.Write("Type a brand: ");
+                        brand = Console.ReadLine();
+                        Console.WriteLine();
+                        Console.Write("Type a price: ");
+                        price = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        Console.Write("Type a model: ");
+                        string model = Console.ReadLine();
+                        Console.WriteLine();
+                        Console.Write("Type a height: ");
+                        int height = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        Console.Write("Type a width: ");
+                        int width = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        Console.Write("Type a length: ");
+                        int length = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        Freezer f = new Freezer(model, height, width, length, brand, price);
+                        array.Add(f);
+                        continue;
+                    case 3:
+                        ArrayFreezer(array);
+                        Console.WriteLine();
+                        continue;
+                }
+            }
+
+        }
+    }
+}
