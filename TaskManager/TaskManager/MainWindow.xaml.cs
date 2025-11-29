502fb46d41bcd64296d4c0be2eb16ba6ce2817fd
Artyrka0312 <stadnikartyr@gmail.com>
2025-11-29 23:41:06 +0200

    Створити віконний додаток який виконуватиме функціонал схожий до системного Task Manager. Вікно повинно містити таблицю яка відображатиме перелік системних процесів. Обов’язковими для показу є основні парамерти: назва процесу, час початку роботи, стан, пріорітет і тд. Завантаження та оновлення розпочнеться автоматично після запуску вікна з певним інтервалом. Користувачу дозволяєть вибрати інтервал (1, 2, 5 сек) або призупинити оновлення. При виборі процеса зі списку користувач може його завершити (kill) або подивитися детальну інформацію. Також надати можливість запустити процес вказавши шлях до запускаючого файла (exe).

---
+++ TaskManager/TaskManager/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+
+namespace SPR_411_TaskManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            grid.ItemsSource = Process.GetProcesses();
+
+            //selected.Kill();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //Process selected = (Process)grid.SelectedItem;
+            //string res = "";//add infirmation about process
+            //MessageBox.Show(selected.ProcessName, "Task Manager", MessageBoxButton.YesNoCancel, MessageBoxImage.Information);
+
+            MessageBox.Show(processName.Text);
+        }
+    }
+}
