1d551b2bc18ec33d806f7f84add26e9000d76bc1
Artyrka0312 <stadnikartyr@gmail.com>
2025-12-17 04:49:38 +0200

    Threads (task 1-5)

---
+++ SP - Threads/SP - Threads/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+
+
+namespace SP___Threads
+{
+    
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            int start, end;
+            Console.Write("Type a start and end of diapason: ");
+            start = int.Parse(Console.ReadLine());
+            end = int.Parse(Console.ReadLine());
+            int numbers = int.Parse(Console.ReadLine());
+            
+            //Thread b = new Thread(ShowDigits2);
+            //b.Start();
+            
+
+            Console.WriteLine("Main is working");
+            
+            //3
+            List<Thread> threads = CreateNewThread(start,end,numbers);
+            foreach (Thread thread in threads) {
+                thread.Join();
+            }
+
+
+
+        }
+
+        
+
+        static List<Thread> CreateNewThread(int start, int end, int count)
+        {
+           List<Thread> threads = new List<Thread>();
+            int range = end - start + 1;
+            int step = range / count;
+            for (int i = 0; i < count; i++)
+            {
+                int l_start = start + i * step;
+                int l_end = (i == count - 1) ? end: l_start + step - 1 ;
+
+                int thread_number = i + 1;
+
+                Thread x = new Thread(() => ShowDigits(new int[] { l_start, l_end, thread_number }));
+                threads.Add(x);
+                x.Start();
+
+            }
+
+            return threads;
+        }
+        static void ShowDigits(object obj)
+        {
+            int[] arr = (int[]) obj;
+            int start = arr[0];
+            int end = arr[1];
+            int numbers = arr[2];
+            Console.Write($"Thread number {numbers}");
+            for (int i = start; i < end + 1; i++)
+            {
+                Console.WriteLine($"the digit is: {i}");
+                Thread.Sleep(50);
+            }
+            
+        }
+
+        
+
+        
+    }
+}
---
+++ SP - Threads/Task_2/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Threading;
+
+namespace Task_2
+{
+    internal class Task_2
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[50];
+            GenerateDigitsToArray(arr);
+            //ArrayToString(arr);
+            Thread min = StartThread(() =>
+            {
+                int min = FindMin(arr);
+                Console.WriteLine($"Min digit of array is: {min}");
+            });
+            
+            Thread max = StartThread(() =>
+            {
+                int max = FindMax(arr);
+                Console.WriteLine($"Max digit of array is: {max}");
+
+            });
+            
+            Thread average = StartThread(() =>
+            {
+                int average = FindAverage(arr);
+                Console.WriteLine($"Average digit of array is {average}");
+            });
+            Thread SaveToFile = StartThread(() =>
+            {
+                SaveArrayToFile(arr);
+                Console.WriteLine("Recording is done!");
+            });
+
+
+            min.Join();
+            max.Join();
+            average.Join();
+            SaveToFile.Join();
+
+            Console.WriteLine("All is done!");
+        }
+        static void SaveArrayToFile(int[] arr)
+        {
+            using (StreamWriter writer = new StreamWriter("array.txt"))
+            {
+                foreach (int i in arr)
+                {
+                    writer.Write($"| {i} |");
+                }
+            }
+        }
+        static void ArrayToString(int[] arr)
+        {
+            foreach (int i in arr)
+            {
+                Console.Write($"| {i} |");
+            }
+        }
+        static void GenerateDigitsToArray(int[] array)
+        {
+            Random x = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = x.Next(1, 50);
+            }
+
+
+        }
+        static Thread StartThread(Action action)
+        {
+            Thread x = new Thread(() => action());
+            x.Start();
+            return x;
+        }
+        static int FindMin(int[] arr)
+        {
+            int min = arr[0];
+            foreach (int i in arr)
+            {
+                if (i < min)
+                {
+                    min = i;
+                }
+            }
+            return min;
+        }
+        static int FindMax(int[] arr)
+        {
+            int min = arr[0];
+            foreach (int i in arr)
+            {
+                if (i > min)
+                {
+                    min = i;
+                }
+            }
+            return min;
+        }
+        static int FindAverage(int[] arr)
+        {
+            int sum = 0;
+            foreach (int i in arr)
+            {
+                sum += i;
+            }
+            int res = sum / arr.Length;
+            return res;
+        }
+    }
+}
