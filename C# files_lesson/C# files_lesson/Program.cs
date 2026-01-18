252e367b8440afdefe69f2a073136f6636512646
Artyrka0312 <stadnikartyr@gmail.com>
2026-01-18 20:56:51 +0200

    Синхронізація потоків. Конкурентний доступ до ресурсів. Interlocked. Monitor

---
+++ C# files_lesson/C# files_lesson/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Specialized;
+using System.Text;
+
+namespace C__files_lesson
+{
+    
+    internal class Program
+    {
+        static void WriteToFile()
+        {
+            string text = "Some text";
+            Stream stream = new FileStream("lorem.txt", FileMode.Create, FileAccess.Write);
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            stream.Write(bytes);
+
+            stream.Close();
+
+        }
+        static void Main(string[] args)
+        {
+            // FileStream
+            
+            Stream stream = new FileStream("lorem.txt", FileMode.Open, FileAccess.Read);
+
+            byte[] buffer = new byte[stream.Length];
+
+            int l = stream.Read(buffer);
+
+            string text = Encoding.UTF8.GetString(buffer,0,l);
+            Console.WriteLine(text);
+
+            stream.Close();
+            
+        }
+    }
+}
---
+++ Thread Synchronization/Thread Synchronization/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Threading;
+using System.Xml;
+namespace Thread_Synchronization
+
+{
+    class Saver
+    {
+        private static int _words;
+        private static int _lines;
+        private static int _punc;
+
+        public static int words => _words;
+        public static int lines => _lines;
+        public static int? punc => _punc;
+        public static void AddWords(int value)
+        => Interlocked.Add(ref _words, value);
+        public static void AddLines(int value)
+        => Interlocked.Add(ref _lines, value);
+        public static void AddPunc(int value)
+            => Interlocked.Add(ref _punc, value);
+
+
+
+
+
+    }
+    internal class Program
+    {
+        static readonly char[] PunctuationMarks =
+        {
+            '.', ',', ';', ':', '–', '—', '‒', '…', '!', '?',
+            '"', '\'', '«', '»',
+            '(', ')', '{', '}', '[', ']', '<', '>', '/'
+        };
+        static void Main(string[] args)
+        {
+            object file_text = File.ReadAllText("data.txt");
+            //words
+            Thread Words = new Thread(new ParameterizedThreadStart(CountWords));
+            Words.Start(file_text);
+            //lines
+            Thread Lines = new Thread(new ParameterizedThreadStart(CountLines));
+            Lines.Start(file_text);
+            //Punc
+            Thread Punc = new Thread(new ParameterizedThreadStart(CountPunc));
+            Punc.Start(file_text);
+            //joins
+            Words.Join();
+            Lines.Join();
+            Punc.Join();
+            //check
+            Console.WriteLine($"Words: {Saver.words}, lines: {Saver.lines}, punc: {Saver.punc}");
+        }
+        
+        static void CountWords(object obj)
+        {
+            int count = 0;
+            string file_text = (string)obj;
+
+            string[] words = file_text.Split(new char[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            count = words.Length;
+            Saver.AddWords(count);
+            
+        }
+        static void CountLines(object obj) {
+
+            string file_text = (string)obj;
+            foreach(var i in file_text)
+            {
+                if(i == '\n')
+                {
+                    Saver.AddLines(1);
+                }
+            }
+        }
+        static void CountPunc(object obj)
+        {
+            int count = 0;
+            string file_text = (string)obj;
+            foreach (var i in file_text)
+            {
+                if (PunctuationMarks.Contains(i))
+                {
+                    count++;
+                }
+            }
+            Saver.AddPunc(count);
+            
+            
+            
+        }
+        
+
+    }
+}
