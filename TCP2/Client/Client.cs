ab3c04b6cdfe56fbf51a81849f865804d5fdfa89
ArtyrST <stadnikartyr@gmail.com>
2026-03-08 17:25:21 +0200

    2

---
+++ TCP2/Client/Client.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ip = "127.0.0.1";
+            const int port = 8080;
+
+            var endPointTCP = new IPEndPoint(IPAddress.Parse(ip), port);
+            var socketTCP = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            
+            Console.WriteLine(@"Введіть варіант(1: повернути дату, 2: повернути час, 0: закрити з'єднання): ");
+            string answer = Console.ReadLine();
+
+            var data = Encoding.UTF8.GetBytes(answer);
+            socketTCP.Connect(endPointTCP);
+            socketTCP.Send(data);
+
+            var buffer = new byte[256];
+            var size = 0;
+            var answerFromServer = new StringBuilder();
+
+            do
+            {
+                size = socketTCP.Receive(buffer);
+                answerFromServer.Append(Encoding.UTF8.GetString(buffer, 0, size));
+
+            } while (socketTCP.Available > 0);
+
+            Console.WriteLine(answerFromServer);
+
+            socketTCP.Shutdown(SocketShutdown.Both);
+            socketTCP.Close();
+
+        }
+    }
+}
---
+++ TCP2/TCP2/Server.cs
@@ -0,0 +1,62 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCP2
+{
+    internal class Program
+    {
+        static void ReturnDate(Socket listener)
+        {
+            Console.WriteLine("Returning date...");
+            listener.Send(Encoding.UTF8.GetBytes(DateTime.Now.ToString("dd.MM.yyyy")));
+        }
+        static void ReturnTime(Socket listener)
+        {
+            Console.WriteLine("Returning time...");
+            listener.Send(Encoding.UTF8.GetBytes(DateTime.Now.ToString("HH:mm:ss")));
+        }
+        static void Main(string[] args)
+        {
+            const string ip = "127.0.0.1";
+            const int port = 8080;
+
+            var endPointTCP = new IPEndPoint(IPAddress.Parse(ip), port);
+            var socketTCP = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socketTCP.Bind(endPointTCP);
+            socketTCP.Listen(5);
+            while (true)
+            {
+                var listener = socketTCP.Accept();
+                var buffer = new byte[1024];
+                int size = 0;
+                var data = new StringBuilder();
+
+                do
+                {
+                    size = listener.Receive(buffer);
+                    data.Append(Encoding.UTF8.GetString(buffer, 0, size));
+
+                } while (listener.Available > 0);
+
+                if (data.Equals("1"))
+                {
+                    ReturnDate(listener);
+                }
+                else if (data.Equals("2"))
+                {
+                    ReturnTime(listener);
+                }
+                else if (data.Equals("0"))
+                {
+                    {
+                        listener.Send(Encoding.UTF8.GetBytes("Done!"));
+                        listener.Shutdown(SocketShutdown.Both);
+                        listener.Close();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
